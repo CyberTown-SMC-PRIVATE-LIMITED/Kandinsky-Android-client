5c9b60144c2848bd5e43f4fd45b1528c4317adb3
dr_bat <kard@bk.ru>
2024-10-11 15:30:39 +0300

    hilt fix!!!

--- app/src/main/java/bat/konst/kandinskyclient/ui/MainActivity.kt
+++ app/src/main/java/bat/konst/kandinskyclient/ui/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import bat.konst.kandinskyclient.ui.navigation.MainNavigation
 import bat.konst.kandinskyclient.ui.theme.KandinskyClientTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalMaterial3Api
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
