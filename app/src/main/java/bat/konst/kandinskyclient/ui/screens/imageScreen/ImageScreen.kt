d2777f9cc293baaea7a10a2b9eaea9ca3e7374e3
dr_bat <kard@bk.ru>
2024-10-18 09:30:23 +0300

    Image - added "Cencored" status support

--- app/src/main/java/bat/konst/kandinskyclient/ui/screens/imageScreen/ImageScreen.kt
+++ app/src/main/java/bat/konst/kandinskyclient/ui/screens/imageScreen/ImageScreen.kt
@@ -1,15 +1,29 @@
 package bat.konst.kandinskyclient.ui.screens.imageScreen
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.hilt.navigation.compose.hiltViewModel
 import bat.konst.kandinskyclient.ui.navigation.Route
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
+import bat.konst.kandinskyclient.R
 import java.io.File
 
 @Composable
@@ -51,9 +65,44 @@ fun ImageView(
         )
         Text(text = "OK")
 
+        // Кнопка "Поделиться"
+        Share(filePath = state.imageBase64, context = LocalContext.current)
 
     }
 }
+
+// Кнопка "Поделиться"
+@Composable
+fun Share(filePath: String, context: Context) {
+    Button(onClick = {
+        // https://readmedium.com/android-kotlin-jetpack-compose-file-sharing-using-fileprovider-and-android-sharesheet-3b8a7b9fb82d
+        // Ко всему этому нужно provider (разрешение) в Manifest
+        // и добавить в res/xml filepaths.xml !!!!
+
+        try {
+            val imageFile = File(filePath)
+            val uri = FileProvider.getUriForFile(
+                context,
+                "bat.konst.kandinskyclient.fileprovider",
+                imageFile
+            )
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, uri)
+                type = "image/jpeg"
+            }
+            sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            ContextCompat.startActivity(context, shareIntent, null)
+        } catch (e: Exception) {
+            Log.d("KandinskyClient", e.toString())
+        }
+    }) {
+        Icon(imageVector = Icons.Default.Share, contentDescription = null)
+        Text(text = stringResource(id = R.string.ps_share), modifier = Modifier.padding(start = 8.dp))
+    }
+}
+
 
 @Composable
 @Preview(showBackground = true)
